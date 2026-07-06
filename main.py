cd5277ecc48f9e11e6930dcca44621bdc6fc4925
Aditya Sinha <23f1000212@ds.study.iitm.ac.in>
2026-07-06 09:29:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ app = FastAPI()
 # ----------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://exam.sanand.workers.dev"
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
